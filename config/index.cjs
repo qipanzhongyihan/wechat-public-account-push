8da05cc522916bd9e6dc46cfd81885086becdf4b
qipanzhongyihan <121001006+qipanzhongyihan@users.noreply.github.com>
2022-12-19 12:05:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,22 +8,22 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx30496a173bba0d9b',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '89839b1abcf3f2523992b25d9a0fbc0d',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '湖北',
+  CITY: '十堰',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '老吴',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o8ahj6M6CJAVMOxhbcHm4MGX9-Cs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '07KSyTUAaENYBTqMglNX6rNRjOn9FOkNzx64Q8T5LUA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
